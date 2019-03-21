20d508d9dd0b31a34bbc6431ee440defaf48b2e0
acordeonl <acordeonlwebmaster@gmail.com>
2019-03-20 19:17:24 -0500

    add search todo list  by tags and title  endpoint

--- routes/v1/CRUD.js
+++ routes/v1/CRUD.js
@@ -4,108 +4,143 @@ var router = express.Router();
 var verifyUser = require('../../middleware/auth/verifyUser');
 var refreshJWT = require('../../middleware/auth/refreshJWT');
 
-var {db}  = require('../../db/models');
-var elementsPerPage = 10;   
+var {
+    db
+} = require('../../db/models');
+var elementsPerPage = 10;
+
+router.get('/query/:query', verifyUser, async function (req, res, next) {
+    try {
+        let page = req.query.page;
+        let entity = req.baseUrl.replace(/^\/v1\//, '');
+        if (page === undefined)
+            page = 0;
+        let payload = await db[entity].findAll({
+            where: {
+                [db.Sequelize.Op.or]: {
+                    title: {
+                        [db.Sequelize.Op.iLike]: `%${req.params.query}%`
+                    },
+                    tags: {
+                        [db.Sequelize.Op.iLike]: `%${req.params.query}%`
+                    }
+                }
+            },
+            order: [
+                ['updatedAt', 'DESC']
+            ]
+        });
+        req.response_data = {
+            payload,
+            status: 200
+        };
+        next();
+    } catch (err) {
+        next(err);
+    }
+}, refreshJWT);
 
 router.get('/list', verifyUser, async function (req, res, next) {
     try {
-        let page = req.query.page ; 
-        let entity = req.baseUrl.replace(/^\/v1\//,'') ; 
-        if(page === undefined)
-            page = 0 ; 
+        let page = req.query.page;
+        let entity = req.baseUrl.replace(/^\/v1\//, '');
+        if (page === undefined)
+            page = 0;
         let data = await db[entity].findAll({
             where: {
                 userId: req.user.id
             },
-            order: [['updatedAt','DESC']]
+            order: [
+                ['updatedAt', 'DESC']
+            ]
             // offset: page * elementsPerPage,
             // limit: elementsPerPage
         });
         req.response_data = {
             status: 200,
-            payload:data
+            payload: data
         };
-        next() ; 
+        next();
     } catch (err) {
         next(err);
     }
-} ,refreshJWT);
+}, refreshJWT);
 
-router.get('/:entityId', verifyUser, async function(req, res, next) {
-    try{
-        let entityId = req.params.entityId ; 
-        let entity = req.baseUrl.replace(/^\/v1\//,'') ;
-        let data = await db[entity].find({where:{
-            id:entityId,
-            userId:req.user.id
-        }}) ; 
+router.get('/:entityId', verifyUser, async function (req, res, next) {
+    try {
+        let entityId = req.params.entityId;
+        let entity = req.baseUrl.replace(/^\/v1\//, '');
+        let data = await db[entity].find({
+            where: {
+                id: entityId,
+                userId: req.user.id
+            }
+        });
         req.response_data = {
             status: 200,
-            payload:data
+            payload: data
         };
-        next() ; 
+        next();
+    } catch (err) {
+        next(err);
     }
-    catch(err){
-        next(err) ; 
-    }
-} , refreshJWT);
+}, refreshJWT);
 
-router.post('/', verifyUser, async function(req, res, next) {
-    try{
-        let entity = req.baseUrl.replace(/^\/v1\//,'') ;
-        req.body.userId = req.user.id ;
-        let data = await db[entity].create(req.body) ; 
+router.post('/', verifyUser, async function (req, res, next) {
+    try {
+        let entity = req.baseUrl.replace(/^\/v1\//, '');
+        req.body.userId = req.user.id;
+        let data = await db[entity].create(req.body);
         req.response_data = {
             status: 200,
-            payload:data
+            payload: data
         };
-        next() ; 
+        next();
+    } catch (err) {
+        next(err);
     }
-    catch(err){
-        next(err) ; 
-    }
-} , refreshJWT);
+}, refreshJWT);
 
 router.put('/:entityId', verifyUser, async function (req, res, next) {
     try {
-        let entityId = req.params.entityId ;
-        let entity = req.baseUrl.replace(/^\/v1\//,'') ; 
+        let entityId = req.params.entityId;
+        let entity = req.baseUrl.replace(/^\/v1\//, '');
         let result = await db[entity].update(
-          req.body,{
-            where: {
-              id:entityId,
-              userId:req.user.id
-            }
-        }) ;
+            req.body, {
+                where: {
+                    id: entityId,
+                    userId: req.user.id
+                }
+            });
         req.response_data = {
-            dev_message:`Updated rows: ${result[0]}`,
+            dev_message: `Updated rows: ${result[0]}`,
             status: 200,
         };
-        next() ;
+        next();
     } catch (err) {
         next(err);
     }
-} , refreshJWT);
+}, refreshJWT);
 
-router.delete('/:entityId' , verifyUser, async function (req, res, next) {
+router.delete('/:entityId', verifyUser, async function (req, res, next) {
     try {
-        var entityId = req.params.entityId ;
-        let entity = req.baseUrl.replace(/^\/v1\//,'') ; 
-        let result = await db[entity].destroy(
-          {where: {
-              id:entityId,
-              userId:req.user.id
-            }}
-        ) ;
+        var entityId = req.params.entityId;
+        let entity = req.baseUrl.replace(/^\/v1\//, '');
+        let result = await db[entity].destroy({
+            where: {
+                id: entityId,
+                userId: req.user.id
+            }
+        });
         req.response_data = {
-            dev_message:`Deleted rows: ${result}`,
+            dev_message: `Deleted rows: ${result}`,
             status: 200
         };
-        next() ; 
+        next();
     } catch (err) {
         next(err);
     }
-} , refreshJWT );
+}, refreshJWT);
 
 
 module.exports = router;
